6f289f897ef97032d31b4eba79ce1ca421be6938
dre264 <adrianstarxs@gmail.com>
2022-09-23 14:34:39 +0700

    Add Tugas7

---
+++ Tugas7/js/script.js
@@ -0,0 +1,112 @@
+
+$( document ).ready(function() {
+
+    $("#ayam").click(function(){
+        $(this).find("img").removeClass("d-none");
+        $("<audio></audio>").attr({
+            'src': "audio/ayam.mp3",
+            'volume': 1,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    });
+    
+    $("#bebek").click(function(){
+        $(this).find("img").removeClass("d-none");
+        $("<audio></audio>").attr({
+            'src': "audio/bebek.mp3",
+            'volume': 1,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    });
+    
+    $("#burung").click(function(){
+        $(this).find("img").removeClass("d-none");
+        $("<audio></audio>").attr({
+            'src': "audio/burung.mp3",
+            'volume': 1,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    });
+    
+    $("#angsa").click(function(){
+        $(this).find("img").removeClass("d-none");
+        $("<audio></audio>").attr({
+            'src': "audio/angsa.mp3",
+            'volume': 1,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    });
+    
+    $("#sapi").click(function(){
+        $(this).find("img").removeClass("d-none");
+        $("<audio></audio>").attr({
+            'src': "audio/sapi.mp3",
+            'volume': 1,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    });
+    
+    $("#domba").click(function(){
+        $(this).find("img").removeClass("d-none");
+        $("<audio></audio>").attr({
+            'src': "audio/domba.mp3",
+            'volume': 1,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    });
+
+    $("#ayam2").click(function(){
+        $(this).find("img").removeClass("d-none");
+        $("<audio></audio>").attr({
+            'src': "audio/ayam.mp3",
+            'volume': 1,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    });
+    
+    $("#kambing").click(function(){
+        $(this).find("img").removeClass("d-none");
+        $("<audio></audio>").attr({
+            'src': "audio/kambing.mp3",
+            'volume': 1,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    });
+    
+    $("#singa").click(function(){
+        $(this).find("img").removeClass("d-none");
+        $("<audio></audio>").attr({
+            'src': "audio/singa.mp3",
+            'volume': 1,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    });
+    
+    $("#trex").click(function(){
+        $(this).find("img").removeClass("d-none");
+        $("<audio></audio>").attr({
+            'src': "audio/trex.mp3",
+            'volume': 1,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    });
+    
+    $("#harimau").click(function(){
+        $(this).find("img").removeClass("d-none");
+        $("<audio></audio>").attr({
+            'src': "audio/harimau.mp3",
+            'volume': 1,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    });
+    
+    $("#serigala").click(function(){
+        $(this).find("img").removeClass("d-none");
+        $("<audio></audio>").attr({
+            'src': "audio/serigala.mp3",
+            'volume': 1,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    });
+
+});   
